09611bdffde0dedbcc0cfe5ec0f4fad4fafca5b9
samuelgiard <samuel.giard@wanadoo.fr>
2018-11-04 16:28:18 +0100

    set userPage link

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to user page</Link>
+    <Link to="/userPage/">Go to user page</Link>
   </Layout>
 )
 
