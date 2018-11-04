4891f3c8938fea7ed4da0d2e9c68cfb0703ef0c4
oliver fu <ofu997@gmail.com>
2018-11-04 15:38:06 -0800

    layout on page 2

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -2,11 +2,25 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
-
+// new import
+import {Button, Container, Row, Col, } from
 const UserPage = () => (
   <Layout>
     <h1>Hi from user profile page</h1>
     <p>here should be a form!</p>
+
+    <img className="img-responsive" src="../images/LOGO.png" alt="logo"/>
+    <Container>
+      <Row>
+        <Col md='3'>
+          // user photo, social media links, manage events here
+        </Col>
+        <Col md='8'>
+          // user description input, skill tags here
+        </Col>
+      </Row>
+    </Container>
+
     <Link to="/page3/">Show me the teams</Link><br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
