a1c89c986234f272c98f01384d5f3a885ac920ff
Shivam Singhania <shivam4663@gmail.com>
2018-11-05 04:32:42 +0530

    improve description

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,9 @@ const IndexPage = () => (
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
-    <h1>Build your team with us</h1>
-    <p>Welcome to your Teamathon site.</p>
+    <h1>Team builder for hackathons</h1>
+    <p>Here you can browse and create teams for your hackathon event.</p>
     <div data-netlify-identity-menu></div>
-    <p>Now go build some awesome teams.</p>
     <Link to="/page2/">Go to user page</Link>
   </Layout>
 )
