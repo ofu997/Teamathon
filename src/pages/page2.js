d5da003e14b60bea66ba6bd283464d1f80e98f24
Shivam Singhania <shivam4663@gmail.com>
2018-11-05 05:05:51 +0530

    improve page2

--- src/pages/page2.js
+++ src/pages/page2.js
@@ -5,8 +5,9 @@ import Layout from '../components/layout'
 
 const UserPage = () => (
   <Layout>
-    <h1>Hi from user profile page</h1>
-    <p>here should be a form!</p>
+    <h1>This is the user profile page</h1>
+    <p> It should contain user's skills(as tags), social media links, timezone, etc. </p>
+    <p> It also lets user manage teams, events and his/her profile. </p>
     <Link to="/page3/">Show me the teams</Link><br/>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
